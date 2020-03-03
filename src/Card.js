c5896c47a03d8452a6619238dd97f4118f073d06
BOURGET Antoine <abourget-prestataire@seres.fr>
2020-03-03 17:47:16 +0100

    seconde commit

---
+++ src/Card.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './Card.css'
+
+const HIDDEN_SYMBOL = '?'
+const Card = ({card, feedback, onClick}) => (
+    <div className={`card ${feedback}`} onClick={() => onClick(card)}>
+        <span className="symbol">
+            {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+        </span>
+    </div>
+)
+
+Card.propTypes = {
+    card: PropTypes.string.isRequired,
+    feedback: PropTypes.oneOf([
+        'hidden',
+        'justMatched',
+        'justMismatched',
+        'visible',
+    ]).isRequired,
+    onClick: PropTypes.func.isRequired,
+}
+
+export default Card
